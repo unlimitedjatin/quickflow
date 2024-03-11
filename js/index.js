0a3b359651fc01c01ab5cf22dd0716348cd5eff7
Jatin Makwana <acceptjatin@gmail.com>
2024-03-11 14:18:36 +0530

    new changes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+  let swiper = new Swiper('.swiper-container', {
+    loop: true, // Enable loop mode
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, // Specify the pagination container
+    },
+  });
+
+  
